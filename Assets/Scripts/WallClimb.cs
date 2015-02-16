e57d28de8feb0ea151f7c5745a208e5ff6154ff2
unknown <K1223262@PPRSB1026-G06.kuds.kingston.ac.uk>
2015-02-16 15:32:06 +0000

    Added Wall Climb script, changed a scene where I tested it. Edited the player moevment controls to be more responsive and less over the top. Added double jump to player movement controlled by a boolean.
    Signed-off-by: unknown <K1223262@PPRSB1026-G06.kuds.kingston.ac.uk>

---
+++ Assets/Scripts/WallClimb.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallClimb : MonoBehaviour {
+
+	PlayerMovement move;
+	void Start()
+	{
+		move = GameObject.Find ("Player 1").GetComponent<PlayerMovement> ();
+	}
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if(col.gameObject.tag == "Player")
+			move.canClimb = true;
+	}
+	void OnTriggerExit2D(Collider2D col)
+	{
+		if(col.gameObject.tag == "Player")
+			move.canClimb = false;
+	}
+}
