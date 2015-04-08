ae7dcff45245decf424580cf49347733c99a02ab
unknown <k0957700@PPRSB1026-G04.kuds.kingston.ac.uk>
2015-04-08 15:52:21 +0100

    Frostbreath stuff
    Signed-off-by: unknown <k0957700@PPRSB1026-G04.kuds.kingston.ac.uk>

--- Assets/Scripts/EnemyMoveAI.cs
+++ Assets/Scripts/EnemyMoveAI.cs
@@ -9,6 +9,8 @@ public class EnemyMoveAI : MonoBehaviour {
 	public Transform headCheck;
 	public LayerMask whatIsPlayer;
 	float radius = 0.2f;
+	public bool frozen;
+	public GameObject ice;
 
 	// Use this for initialization
 	void Start () {
@@ -26,20 +28,22 @@ public class EnemyMoveAI : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		dist = Vector2.Distance (transform.position, target.transform.position);
-		if (dist <= viewDist && dist >= stayDist && target.transform.position.y < transform.position.y + 0.3f) {
-			if(target.transform.position.x > transform.position.x) {
-				move = 1;
-			} else if (target.transform.position.x < transform.position.x) {
-				move = -1;
-			} else {
-				move = 0;
-			}
-			rigidbody2D.velocity = new Vector3 (move * speed, rigidbody2D.velocity.y);
-		}
+		if (frozen == false) {
+						if (dist <= viewDist && dist >= stayDist && target.transform.position.y < transform.position.y + 0.3f) {
+								if (target.transform.position.x > transform.position.x) {
+										move = 1;
+								} else if (target.transform.position.x < transform.position.x) {
+										move = -1;
+								} else {
+										move = 0;
+								}
+								rigidbody2D.velocity = new Vector3 (move * speed, rigidbody2D.velocity.y);
+						}
+				}
 	}
 
 	void OnCollisionEnter2D(Collision2D coll) {
-		if (coll.gameObject.tag == "Player") {
+		if (coll.gameObject.tag == "Player" && frozen == false) {
 			PlayerMovement movement = (PlayerMovement)target.GetComponent(typeof(PlayerMovement));
 			if(headButt){//target.transform.position.y > transform.position.y + 0.15f) {
 				movement.bounce(3);
@@ -48,6 +52,17 @@ public class EnemyMoveAI : MonoBehaviour {
 			} else if (target.transform.position.x < transform.position.x-0.1f) {
 				movement.bounce(2);
 			}
+		}
+	}
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.name == "BreathRight" && frozen == false) {
+						frozen = true;
+			(Instantiate (ice, transform.position, transform.rotation) as GameObject).transform.parent =  gameObject.transform;
+
+				}
+		if (other.name == "BreathLeft"&& frozen == false) {
+			(Instantiate (ice, transform.position, transform.rotation) as GameObject).transform.parent =  gameObject.transform;
+			frozen = true;
 		}
 	}
 }
